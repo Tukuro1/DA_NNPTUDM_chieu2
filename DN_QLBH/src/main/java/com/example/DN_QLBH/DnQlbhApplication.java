c55fdfa8e1452858bfd24f24ab2180a51b1c9173
Nguyễn Như Tú <tunguyennhuxyz@gmail.com>
2025-04-06 02:49:45 +0700

    API+Stringboot

---
+++ DN_QLBH/src/main/java/com/example/DN_QLBH/DnQlbhApplication.java
@@ -0,0 +1,13 @@
+package com.example.DN_QLBH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnQlbhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnQlbhApplication.class, args);
+	}
+
+}
---
+++ DN_QLBH/src/test/java/com/example/DN_QLBH/DnQlbhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DN_QLBH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnQlbhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

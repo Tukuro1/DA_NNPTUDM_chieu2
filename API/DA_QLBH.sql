9b1888f55101aebb0c1a3e452bc4e54ef9fcfb9e
Nguyễn Như Tú <tunguyennhuxyz@gmail.com>
2025-04-10 12:12:56 +0700

    update_csdl

--- API/DA_QLBH.sql
+++ API/DA_QLBH.sql
@@ -1,6 +1,8 @@
 -- Active: 1743886746588@@127.0.0.1@1433@DA_QLBH
 CREATE DATABASE DA_QLBH
 
+GO
+
 USE DA_QLBH
 
 GO
@@ -18,11 +20,13 @@ GO
 -- Thêm dữ liệu vào bảng Products
 INSERT INTO Products (Name, Description, Price, Stock, ImageURL, Size, Color, CategoryID)
 VALUES 
-('Áo sơ mi trắng', 'Áo sơ mi trắng cao cấp', 250000, 50, 'https://example.com/images/ao-so-mi-trang.jpg', 'M', 'Trắng', 1),
-('Áo thun đen', 'Áo thun đen thoáng mát', 150000, 100, 'https://example.com/images/ao-thun-den.jpg', 'L', 'Đen', 1),
-('Quần jean xanh', 'Quần jean xanh thời trang', 350000, 30, 'https://example.com/images/quan-jean-xanh.jpg', '32', 'Xanh', 2),
-('Giày thể thao', 'Giày thể thao năng động', 500000, 20, 'https://example.com/images/giay-the-thao.jpg', '42', 'Trắng', 3),
-('Mũ lưỡi trai', 'Mũ lưỡi trai thời trang', 100000, 70, 'https://example.com/images/mu-luoi-trai.jpg', 'Free Size', 'Đen', 4);
+('Áo sơ mi trắng', 'Áo sơ mi trắng cao cấp', 250000, 50, 'https://sbhtailor.vn/wp-content/uploads/2022/08/mix-ao-so-mi-trang-nam-1.jpg', 'M', 'Trắng', 1),
+('Áo thun đen', 'Áo thun đen thoáng mát', 150000, 100, 'https://down-vn.img.susercontent.com/file/b2ec0cc362006e8dddde77df97564aa7', 'L', 'Đen', 1),
+('Quần jean xanh', 'Quần jean xanh thời trang', 350000, 30, 'https://pubcdn.ivymoda.com/files/news/2023/10/27/c284d544a99b014031efd25821f60441.jpg', '32', 'Xanh', 2),
+('Giày thể thao', 'Giày thể thao năng động', 500000, 20, 'https://bizweb.dktcdn.net/thumb/1024x1024/100/449/472/products/vn-11134207-7r98o-lo2ho07q4ghmea-1699958923523.jpg?v=1699959270613', '42', 'Trắng', 3),
+('Mũ lưỡi trai', 'Mũ lưỡi trai thời trang', 100000, 70, 'https://product.hstatic.net/1000312752/product/68e33fe9f15c22602b5ff90da8553b5a3cfe2a31e9c2b5c4da3371b38624078f777ed3_b603631672a742c583524309180ecea1.jpg', 'Free Size', 'Đen', 4),
+('Áo sơ mi đen dài tay', 'Áo sơ mi đen dài tay cao cấp', 300000, 50, 'https://sbhtailor.vn/wp-content/uploads/2024/01/1-1-1.png', 'L', 'Đen', 1);
+
 
 GO
 
